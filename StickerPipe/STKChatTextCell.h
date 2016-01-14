22375c5a69ca6ab63b4e5b2846e9100e4e1c0b1a
Olya Lutsyk <olyalutsyk@MacBook-Pro-Olya.local>
2016-01-14 17:10:50 +0200

    Add STKChatTextCell class

---
+++ StickerPipe/STKChatTextCell.h
@@ -0,0 +1,16 @@
+//
+//  STKChatTextTableViewCell.h
+//  StickerPipe
+//
+//  Created by Olya Lutsyk on 1/14/16.
+//  Copyright © 2016 908 Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface STKChatTextCell : UITableViewCell
+
+@property (weak, nonatomic) IBOutlet UILabel *textMessage;
+- (void)fillWithTextMessage:(NSString *)message;
+
+@end
---
+++ StickerPipe/STKChatTextCell.m
@@ -0,0 +1,26 @@
+//
+//  STKChatTextTableViewCell.m
+//  StickerPipe
+//
+//  Created by Olya Lutsyk on 1/14/16.
+//  Copyright © 2016 908 Inc. All rights reserved.
+//
+
+#import "STKChatTextCell.h"
+
+@implementation STKChatTextTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+- (void)fillWithTextMessage:(NSString *)message {
+    self.textMessage.text = message;
+}
+
+@end
