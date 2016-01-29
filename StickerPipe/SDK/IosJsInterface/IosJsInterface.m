4d603cc4ecd0528296b0c3af897c6fd4f4ddb74d
Olya Lutsyk <olyalutsyk@MacBook-Pro-Olya.local>
2016-01-29 17:33:29 +0200

    Add IosJsInterface class

---
+++ StickerPipe/SDK/IosJsInterface/IosJsInterface.m
@@ -0,0 +1,25 @@
+//
+//  IosJsInterface.m
+//  StickerPipe
+//
+//  Created by Olya Lutsyk on 1/29/16.
+//  Copyright © 2016 908 Inc. All rights reserved.
+//
+
+#import "IosJsInterface.h"
+
+@implementation IosJsInterface
+
+- (void)showCollections {
+    NSLog(@"showCollections");
+}
+
+- (void)purchasePack:(NSString *)packTitle :(NSString *)packName :(NSString *)packPrice {
+    
+}
+
+- (void)setInProgress:(BOOL)show {
+    
+}
+
+@end
