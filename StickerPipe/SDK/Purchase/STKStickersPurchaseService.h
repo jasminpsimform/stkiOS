17bacdccf7f7351277fb9dae96f004f7d4785525
Olya Lutsyk <olyalutsyk@MacBook-Pro-Olya.local>
2016-02-16 17:07:17 +0200

    Add RMStore pod

---
+++ StickerPipe/SDK/Purchase/STKStickersPurchaseService.h
@@ -0,0 +1,13 @@
+//
+//  STKStickersPurchaseService.h
+//  StickerPipe
+//
+//  Created by Olya Lutsyk on 2/16/16.
+//  Copyright © 2016 908 Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface STKStickersPurchaseService : NSObject
+
+@end
---
+++ StickerPipe/SDK/Purchase/STKStickersPurchaseService.m
@@ -0,0 +1,13 @@
+//
+//  STKStickersPurchaseService.m
+//  StickerPipe
+//
+//  Created by Olya Lutsyk on 2/16/16.
+//  Copyright © 2016 908 Inc. All rights reserved.
+//
+
+#import "STKStickersPurchaseService.h"
+
+@implementation STKStickersPurchaseService
+
+@end
