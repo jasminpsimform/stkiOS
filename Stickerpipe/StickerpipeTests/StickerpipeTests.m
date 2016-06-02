28a08fd11e6f910b785bc4aa4060527034f6be00
Alexander908 <alexander908@Alexander908s-MacBook-Pro.local>
2016-06-02 13:40:41 +0300

    add Stickerpipe framework

---
+++ Stickerpipe/StickerpipeTests/StickerpipeTests.m
@@ -0,0 +1,39 @@
+//
+//  StickerpipeTests.m
+//  StickerpipeTests
+//
+//  Created by Alexander908 on 6/2/16.
+//  Copyright © 2016 Alexander908. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface StickerpipeTests : XCTestCase
+
+@end
+
+@implementation StickerpipeTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
