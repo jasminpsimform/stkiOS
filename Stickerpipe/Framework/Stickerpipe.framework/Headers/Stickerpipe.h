168da75608543d03df23a7a8536cc5eadf33c122
Alexander908 <alexander908@Alexander908s-MacBook-Pro.local>
2016-08-01 15:08:17 +0300

    Release 0.3.15

--- Stickerpipe/Framework/Stickerpipe.framework/Headers/Stickerpipe.h
+++ Stickerpipe/Framework/Stickerpipe.framework/Headers/Stickerpipe.h
@@ -16,8 +16,6 @@ FOUNDATION_EXPORT const unsigned char StickerpipeVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <Stickerpipe/PublicHeader.h>
 
-
-#import <UIKit/UIKit.h>
 #import "STKStickersManager.h"
 #import "UIImageView+Stickers.h"
 #import "STKStickerController.h"
@@ -26,53 +24,6 @@ FOUNDATION_EXPORT const unsigned char StickerpipeVersionString[];
 #import "STKImageManager.h"
 #import "STKUtility.h"
 #import "STKShowStickerButton.h"
-
 #import "UIImage+Tint.h"
 #import "STKStickersPurchaseService.h"
-
-#import "_STKStatistic.h"
-#import "_STKSticker.h"
-#import "_STKStickerPack.h"
-#import "NSManagedObject+STKAdditions.h"
-#import "NSManagedObjectContext+STKAdditions.h"
-#import "NSPersistentStoreCoordinator+STKAdditions.h"
-#import "NSString+STKMD5.h"
-#import "SKProduct+STKStickerSKProduct.h"
-#import "STKAnalyticsAPIClient.h"
-#import "STKAnalyticService.h"
-#import "STKApiAbstractService.h"
-#import "STKApiKeyManager.h"
-#import "STKCoreDataService.h"
-#import "STKEmptyRecentCell.h"
-#import "STKInAppProductsManager.h"
-#import "STKOrientationNavigationController.h"
-#import "STKStatistic.h"
-#import "STKSticker.h"
-#import "STKStickerDelegateManager.h"
-#import "STKStickerHeaderCell.h"
-#import "STKStickerHeaderDelegateManager.h"
-#import "STKStickerObject.h"
-#import "STKStickerPack.h"
-#import "STKStickerPackObject.h"
-#import "STKStickerPackProtocol.h"
-#import "STKStickerPipe.h"
-#import "STKStickerProtocol.h"
-#import "STKStickersApiService.h"
-#import "STKStickersCache.h"
-#import "STKStickersEntityService.h"
-#import "STKStickerSettingsCell.h"
-#import "STKStickersSeparator.h"
-#import "STKStickersSerializer.h"
-#import "STKStickersSettingsViewController.h"
-#import "STKStickersShopJsInterface.h"
-#import "STKStickersShopViewController.h"
-#import "STKStickerViewCell.h"
-#import "STKTableViewDataSource.h"
-#import "STKUUIDManager.h"
-#import "UIImage+CustomBundle.h"
-
-#import "STKSearchDelegateManager.h"
-#import "STKSearchManager.h"
-#import "STKSearchModel.h"
-
 
