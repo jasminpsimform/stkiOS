59362fbb2a3138429f73d2012dd93c2b0c4d619a
vlad <vladhatko@gmail.com>
2017-05-04 13:55:22 +0200

    fast fix

--- SDK/Core/UIImage+CustomBundle.m
+++ SDK/Core/UIImage+CustomBundle.m
@@ -7,6 +7,7 @@
 //
 
 #import "UIImage+CustomBundle.h"
+#import "STKSticker.h"
 
 #define SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(v)  ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] != NSOrderedAscending)
 
